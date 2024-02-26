91b63ca946915aa5d476b0f0fd81799988f6fcc1
lukewire <lukewire129@gmail.com>
2024-02-26 10:52:55 +0900

    초기 커밋.

---
+++ TextConverterSample/Converter/TestConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace TextConverterSample.Converter;
+
+public class TestConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is string str)
+        {
+            return $"{str} + 1";
+        }
+
+        return (string)"";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException ();
+    }
+}
---
+++ TextConverterSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextConverterSample.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty] string abc;
+        public MainViewModel()
+        {
+            Abc = "HIHIHI";
+        }
+
+        int aa = 1;
+        [RelayCommand]
+        private void aaaa()
+        {
+            if (aa == 1)
+            {
+                Abc = "HIHIHI1";
+                aa = 0;
+            }
+
+            else
+            {
+                Abc = "HIHIHI";
+                aa = 1;
+            }
+        }
+    }
+}
